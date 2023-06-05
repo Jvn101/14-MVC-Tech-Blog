86befe15ab52eab748ca0023e9a1d053f9ec1a9a
J Neal <jasmineneal101@gmail.com>
2023-06-05 15:13:00 +1000

    resolved redirect issue with login

--- controllers/api/userRoutes.js
+++ controllers/api/userRoutes.js
@@ -40,9 +40,10 @@ router.post('/login', async (req, res) => {
       req.session.user_id = userData.id;
       req.session.logged_in = true;
       res.redirect("/dashboard");
-      
-      res.json({ username: userData, message: 'You are now logged in!' });
     });
+
+
+     //res.json({ username: userData, message: 'You are now logged in!' });
 
   } catch (err) {
     res.status(400).json(err);
