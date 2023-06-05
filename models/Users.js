6f2338f4f2e97f8af3966e2e8933326695fcdbb2
J Neal <jasmineneal101@gmail.com>
2023-06-05 13:33:31 +1000

    error with login-route

--- models/Users.js
+++ models/Users.js
@@ -19,13 +19,9 @@ Users.init(
     username: {
       type: DataTypes.STRING,
       allowNull: false,
-    },
-    email: {
-      type: DataTypes.STRING,
-      allowNull: false,
       unique: true,
       validate: {
-        isEmail: true,
+        isUsername: true,
       },
     },
     password: {
